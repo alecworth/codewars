a8fb8ecf50fb1a08924d277b33a999e28976dd1c
alec <alecworth18@hotmail.com>
2024-05-11 10:47:35 -0700

    may 11 daily codewars

---
+++ 7kyu/fizzBuzz.js
@@ -0,0 +1,16 @@
+// Return an array
+function fizzbuzz(n) {
+    let newArr = []
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        newArr.push('FizzBuzz')
+      } else if (i % 3 === 0) {
+        newArr.push('Fizz')
+      } else if (i % 5 === 0) {
+        newArr.push('Buzz')
+      } else {
+        newArr.push(i)
+      }
+    }
+    return newArr
+}
